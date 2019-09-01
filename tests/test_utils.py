6e2d534325d1e2deaa93a8ab86c0f6ca8174e307
WHGhost <wghosth@gmail.com>
2019-09-01 19:15:00 +0200

    Tests for stegsuite.utils.steg_alterbits

---
+++ tests/test_utils.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+from stegsuite.utils import *
+
+class Func_steg_alterbits_TestCase(TestCase):
+
+    def test_int(self):
+        test_string = "1234567890AZERTYUIOPQSDFGHJKLMWX"
+        test_bytes = b'Super Secret Message of the Doom'
+        b = bytearray(test_string.encode('utf-8'))
+        steg_alterbits(b, test_bytes, 255),
+        self.assertEqual(b, test_bytes)
+
+    def test_iter(self):
+        test_string = "1234567890AZERTYUIOPQSDFGHJKLMWX"
+        test_bytes = b'Super Secret Message of the Doom'
+        b = bytearray(test_string.encode('utf-8'))
+        steg_alterbits(b, test_bytes, [255, ] * len(b)),
+        self.assertEqual(b, test_bytes)
+
+    def test_func(self):
+        test_string = "1234567890AZERTYUIOPQSDFGHJKLMWX"
+        test_bytes = b'Super Secret Message of the Doom'
+        b = bytearray(test_string.encode('utf-8'))
+        steg_alterbits(b, test_bytes, lambda a, b: 255),
+        self.assertEqual(b, test_bytes)
+
+    def test_nullmask(self):
+        test_string = "1234567890AZERTYUIOPQSDFGHJKLMW"
+        test_bytes = b'Super Secret Message of the Doom'
+        b = bytearray(test_string.encode('utf-8'))
+        with self.assertRaises(CoverError):
+            steg_alterbits(b, test_bytes, 0)
+
+    def test_covertype(self):
+        with self.assertRaises(TypeError):
+            steg_alterbits(b'test', b'Lolmdr', 1)
+
+    def test_streamtype(self):
+        with self.assertRaises(TypeError):
+            steg_alterbits(bytearray(b'test'), "", 1)
+
+    def test_masktype(self):
+        with self.assertRaises(TypeError):
+            steg_alterbits(bytearray(b'test'), b"A cool place to hide things", "3")
